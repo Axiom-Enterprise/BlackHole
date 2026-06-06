e8d61d6ca3041b3bff63c4448e33000256524395
Carlo Maria Cardí <112445885+MathsAnalysis@users.noreply.github.com>
2026-06-06 12:22:52 +0200

    refactor: migrate server GUI colors from Bungee ChatColor to Adventure
    Replace net.md_5.bungee.api.ChatColor with Adventure NamedTextColor in
    the Swing server GUI, carrying explicit legacy section codes.
    
    - GUIColor: NamedTextColor + legacyCode, BY_TEXT_COLOR lookup
    - JColorTextPane: consume the Adventure-based color API

--- purpur-server/src/main/java/org/purpurmc/purpur/gui/GUIColor.java
+++ purpur-server/src/main/java/org/purpurmc/purpur/gui/GUIColor.java
@@ -1,36 +1,40 @@
 package org.purpurmc.purpur.gui;
 
-import net.md_5.bungee.api.ChatColor;
+import net.kyori.adventure.text.format.NamedTextColor;
+import net.kyori.adventure.text.format.TextColor;
 
 import java.awt.Color;
 import java.util.HashMap;
 import java.util.Map;
 
 public enum GUIColor {
-    BLACK(ChatColor.BLACK, new Color(0x000000)),
-    DARK_BLUE(ChatColor.DARK_BLUE, new Color(0x0000AA)),
-    DARK_GREEN(ChatColor.DARK_GREEN, new Color(0x00AA00)),
-    DARK_AQUA(ChatColor.DARK_AQUA, new Color(0x009999)),
-    DARK_RED(ChatColor.DARK_RED, new Color(0xAA0000)),
-    DARK_PURPLE(ChatColor.DARK_PURPLE, new Color(0xAA00AA)),
-    GOLD(ChatColor.GOLD, new Color(0xBB8800)),
-    GRAY(ChatColor.GRAY, new Color(0x888888)),
-    DARK_GRAY(ChatColor.DARK_GRAY, new Color(0x444444)),
-    BLUE(ChatColor.BLUE, new Color(0x5555FF)),
-    GREEN(ChatColor.GREEN, new Color(0x55FF55)),
-    AQUA(ChatColor.AQUA, new Color(0x55DDDD)),
-    RED(ChatColor.RED, new Color(0xFF5555)),
-    LIGHT_PURPLE(ChatColor.LIGHT_PURPLE, new Color(0xFF55FF)),
-    YELLOW(ChatColor.YELLOW, new Color(0xFFBB00)),
-    WHITE(ChatColor.WHITE, new Color(0xBBBBBB));
 
-    private final ChatColor chat;
+    BLACK(NamedTextColor.BLACK, '0', new Color(0x000000)),
+    DARK_BLUE(NamedTextColor.DARK_BLUE, '1', new Color(0x0000AA)),
+    DARK_GREEN(NamedTextColor.DARK_GREEN, '2', new Color(0x00AA00)),
+    DARK_AQUA(NamedTextColor.DARK_AQUA, '3', new Color(0x009999)),
+    DARK_RED(NamedTextColor.DARK_RED, '4', new Color(0xAA0000)),
+    DARK_PURPLE(NamedTextColor.DARK_PURPLE, '5', new Color(0xAA00AA)),
+    GOLD(NamedTextColor.GOLD, '6', new Color(0xBB8800)),
+    GRAY(NamedTextColor.GRAY, '7', new Color(0x888888)),
+    DARK_GRAY(NamedTextColor.DARK_GRAY, '8', new Color(0x444444)),
+    BLUE(NamedTextColor.BLUE, '9', new Color(0x5555FF)),
+    GREEN(NamedTextColor.GREEN, 'a', new Color(0x55FF55)),
+    AQUA(NamedTextColor.AQUA, 'b', new Color(0x55DDDD)),
+    RED(NamedTextColor.RED, 'c', new Color(0xFF5555)),
+    LIGHT_PURPLE(NamedTextColor.LIGHT_PURPLE, 'd', new Color(0xFF55FF)),
+    YELLOW(NamedTextColor.YELLOW, 'e', new Color(0xFFBB00)),
+    WHITE(NamedTextColor.WHITE, 'f', new Color(0xBBBBBB));
+
+    private final NamedTextColor textColor;
+    private final char legacyCode;
     private final Color color;
 
-    private static final Map<ChatColor, GUIColor> BY_CHAT = new HashMap<>();
+    private static final Map<NamedTextColor, GUIColor> BY_TEXT_COLOR = new HashMap<>();
 
-    GUIColor(ChatColor chat, Color color) {
-        this.chat = chat;
+    GUIColor(NamedTextColor textColor, char legacyCode, Color color) {
+        this.textColor = textColor;
+        this.legacyCode = legacyCode;
         this.color = color;
     }
 
@@ -38,21 +42,25 @@ public enum GUIColor {
         return color;
     }
 
-    public ChatColor getChatColor() {
-        return chat;
+    public NamedTextColor getTextColor() {
+        return textColor;
     }
 
     public String getCode() {
-        return chat.toString();
+        return "§" + legacyCode;
     }
 
-    public static GUIColor getColor(ChatColor chat) {
-        return BY_CHAT.get(chat);
+    public static GUIColor getColor(TextColor textColor) {
+        if (textColor instanceof NamedTextColor namedTextColor) {
+            return BY_TEXT_COLOR.get(namedTextColor);
+        }
+
+        return null;
     }
 
     static {
         for (GUIColor color : values()) {
-            BY_CHAT.put(color.chat, color);
+            BY_TEXT_COLOR.put(color.textColor, color);
         }
     }
 }
--- purpur-server/src/main/java/org/purpurmc/purpur/gui/JColorTextPane.java
+++ purpur-server/src/main/java/org/purpurmc/purpur/gui/JColorTextPane.java
@@ -1,12 +1,14 @@
 package org.purpurmc.purpur.gui;
 
 import com.google.common.collect.Sets;
-import javax.swing.UIManager;
+import javax.swing.*;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.Style;
+import net.kyori.adventure.text.format.TextDecoration;
+import net.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer;
 import net.md_5.bungee.api.chat.BaseComponent;
 import net.md_5.bungee.api.chat.TextComponent;
 
-import javax.swing.JTextPane;
-import javax.swing.Timer;
 import javax.swing.text.AttributeSet;
 import javax.swing.text.BadLocationException;
 import javax.swing.text.SimpleAttributeSet;
@@ -17,67 +19,67 @@ import java.util.Set;
 public class JColorTextPane extends JTextPane {
     private static final GUIColor DEFAULT_COLOR;
     static {
-        DEFAULT_COLOR = UIManager.getSystemLookAndFeelClassName().equals("com.sun.java.swing.plaf.gtk.GTKLookAndFeel")
-            ? GUIColor.WHITE : GUIColor.BLACK;
+        DEFAULT_COLOR = UIManager.getSystemLookAndFeelClassName().equals("com.sun.java.swing.plaf.gtk.GTKLookAndFeel") ? GUIColor.WHITE : GUIColor.BLACK;
+    }
+
+    public JColorTextPane(){
+        new Timer(500, _ -> {
+            SYNC_BLINK = !SYNC_BLINK;
+            BLINKS.forEach(this::blink);
+        }).start();
     }
 
 
     public void append(String msg) {
-        // TODO: update to use adventure instead
-        BaseComponent[] components = TextComponent.fromLegacyText(DEFAULT_COLOR.getCode() + msg, DEFAULT_COLOR.getChatColor());
-        for (BaseComponent component : components) {
-            String text = component.toPlainText();
-            if (text == null || text.isEmpty()) {
-                continue;
+        Component component = LegacyComponentSerializer.legacySection().deserialize(DEFAULT_COLOR.getCode() + msg);
+        append(component, Style.empty());
+    }
+
+    private void append(Component component, Style parentStyle) {
+        Style style = parentStyle.merge(component.style());
+
+        for (Component child : component.children()) {
+            append(child, style);
+        }
+    }
+
+    private void appendText(String text, Style style) {
+        GUIColor guiColor = GUIColor.getColor(style.color());
+        if (guiColor == null) {
+            guiColor = DEFAULT_COLOR;
+        }
+
+        StyleContext context = StyleContext.getDefaultStyleContext();
+        AttributeSet attr = context.addAttribute(SimpleAttributeSet.EMPTY, StyleConstants.Foreground, guiColor.getColor());
+        attr = context.addAttribute(attr, StyleConstants.CharacterConstants.Bold,
+            style.decoration(TextDecoration.BOLD) == TextDecoration.State.TRUE || guiColor != DEFAULT_COLOR);
+        attr = context.addAttribute(attr, StyleConstants.CharacterConstants.Italic,
+            style.decoration(TextDecoration.ITALIC) == TextDecoration.State.TRUE);
+        attr = context.addAttribute(attr, StyleConstants.CharacterConstants.Underline,
+            style.decoration(TextDecoration.UNDERLINED) == TextDecoration.State.TRUE);
+        attr = context.addAttribute(attr, StyleConstants.CharacterConstants.StrikeThrough,
+            style.decoration(TextDecoration.STRIKETHROUGH) == TextDecoration.State.TRUE);
+
+        try {
+            int pos = getDocument().getLength();
+            getDocument().insertString(pos, text, attr);
+
+            if (style.decoration(TextDecoration.OBFUSCATED) == TextDecoration.State.TRUE) {
+                Blink blink = new Blink(pos, text.length(), attr,
+                    context.addAttribute(attr, StyleConstants.Foreground, getBackground()));
+                BLINKS.add(blink);
             }
-
-            GUIColor guiColor = GUIColor.getColor(component.getColor());
-
-            StyleContext context = StyleContext.getDefaultStyleContext();
-            AttributeSet attr = context.addAttribute(SimpleAttributeSet.EMPTY, StyleConstants.Foreground, guiColor.getColor());
-            attr = context.addAttribute(attr, StyleConstants.CharacterConstants.Bold, component.isBold() || guiColor != DEFAULT_COLOR);
-            attr = context.addAttribute(attr, StyleConstants.CharacterConstants.Italic, component.isItalic());
-            attr = context.addAttribute(attr, StyleConstants.CharacterConstants.Underline, component.isUnderlined());
-            attr = context.addAttribute(attr, StyleConstants.CharacterConstants.StrikeThrough, component.isStrikethrough());
-            //attr = context.addAttribute(attr, StyleConstants.CharacterConstants.Blink, component.isObfuscated()); // no such thing as Blink, sadly
-
-            try {
-                int pos = getDocument().getLength();
-                getDocument().insertString(pos, text, attr);
-
-                if (component.isObfuscated()) {
-                    // dirty hack to blink some text
-                    Blink blink = new Blink(pos, text.length(), attr, context.addAttribute(attr, StyleConstants.Foreground, getBackground()));
-                    BLINKS.add(blink);
-                }
-            } catch (BadLocationException ignore) {
-            }
+        } catch (BadLocationException exception) {
+            exception.printStackTrace(System.err);
         }
     }
 
     private static final Set<Blink> BLINKS = Sets.newHashSet();
     private static boolean SYNC_BLINK;
 
-    static {
-        new Timer(500, e -> {
-            SYNC_BLINK = !SYNC_BLINK;
-            BLINKS.forEach(Blink::blink);
-        }).start();
+    public void blink(Blink blink) {
+        getStyledDocument().setCharacterAttributes(blink.start(), blink.length(), SYNC_BLINK ? blink.attr() : blink.attr2(), true);
     }
 
-    public class Blink {
-        private final int start, length;
-        private final AttributeSet attr1, attr2;
-
-        private Blink(int start, int length, AttributeSet attr1, AttributeSet attr2) {
-            this.start = start;
-            this.length = length;
-            this.attr1 = attr1;
-            this.attr2 = attr2;
-        }
-
-        private void blink() {
-            getStyledDocument().setCharacterAttributes(start, length, SYNC_BLINK ? attr1 : attr2, true);
-        }
-    }
+    public record Blink(int start, int length, AttributeSet attr, AttributeSet attr2) {}
 }
